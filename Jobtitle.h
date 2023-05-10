9110990305a7ba3c7fc7dae901df98d8c7ca50ae
Dvalera22 <99306238+Dvalera22@users.noreply.github.com>
2023-05-10 11:19:45 -0700

    Update Jobtitle.h

--- Jobtitle.h
+++ Jobtitle.h
@@ -1,1 +1,23 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#ifndef JOBTITLE_H
+#define JOBTITLE_H
 
+using namespace std;
+
+class Jobtitle {
+private:
+	string jobName;
+	string jobPosition;
+
+public:
+	Jobtitle(string name, string position);
+	void setJobName(string name);
+	void setPosition(string position);
+
+	string getJobName() { return (jobName); }
+	string getJobPosition() { return (jobPosition); }
+};
+#endif
