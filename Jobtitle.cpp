6464c6b6556ff4d21e6795ac81c1316cb125c850
Dvalera22 <99306238+Dvalera22@users.noreply.github.com>
2023-05-10 11:32:03 -0700

    Update Jobtitle.cpp

--- Jobtitle.cpp
+++ Jobtitle.cpp
@@ -1,1 +1,19 @@
+#include "Jobtitle.h"
+#include <iostream>
+using namespace std;
 
+//the constructor for jobtitle
+Jobtitle::Jobtitle(string name, string position) {
+    jobName = name;
+    jobPosition = position;
+}
+
+//setter for job name
+void Jobtitle::setJobName(string name) {
+    
+}
+
+// setter for jobPosition
+void Jobtitle::setPosition(string position) {
+    jobPosition = position;
+}
