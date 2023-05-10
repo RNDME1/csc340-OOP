9445fa4d3d9d099e7226ecf0e30d3873c4d76570
zhengcalvin123 <54451643+zhengcalvin123@users.noreply.github.com>
2023-05-09 23:44:44 -0700

    Update Questionnaire.cpp

--- Questionnaire.cpp
+++ Questionnaire.cpp
@@ -1,45 +1,74 @@
 #include <iostream>
 #include <string>
-
+#include "Questionaire.h"
+#include <vector>
 using namespace std;
 
 
-class User {
+class Questionaire {
 public:
   int numProjects;
   int languageEXP;
   int yearsExperience;
-
+  Questionaire(vector<int> answers) {
+  numProjects = answers[0];
+  languageEXP = answers[1];
+  yearsExperience = answers[2];
+}
   void askQuestions() {
-    cout << "How many projects do you have? ";
-    cout << "0" << endl;
-    cout << "1." << endl;
-    cout << "2." << endl;
-    cout << "3." << endl;
-    cout << "4." << endl;
-    cout << "4+" << endl;
-    cin >> numProjects;
+    vector<vector<string>> questions_and_answers = {
+        {"How many projects do you have?", "0", "1.", "2.", "3.", "4.", "4+"},
+        {"Do you have any experience using Java, C++ , or Python?", 
+         "1. I have experience using Java", 
+         "2. I have experience using C++", 
+         "3. I have experience using Python", 
+         "4. I have experience using Java and C++", 
+         "5. I have experience using Python and C++", 
+         "6. I have experience using Java and Python", 
+         "7. I have experience using C++ and Python",
+         "8. I have experience using Java and Python and C++"},
+        {"How many years of experience do you have as a software engineer?",
+         "1. Less than one year",
+         "2. One year",
+         "3. Two years",
+         "4. Three years",
+         "5. Over three years",
+         "6. No experience"}
+    };
 
-    cout << "Do you have any experience using Java, C++ , or Python?" << endl;
-    cout << "1. I have experience using Java "<< endl;
-    cout << "2. I have experience using C++" << endl;
-    cout << "3. I have experience using Python" << endl;
-    cout << "4. I have experience using Java and C++" << endl;
-    cout << "5. I have experience using Python and C++" << endl;
-    cout << "6. I have experience using Java and Python" << endl;
-    cout << "7. I have experience using C++ and Python" <<endl;
-    cout << "8. I have experience using Java and Python and C++" << endl;
+    vector<int> answers(3);
 
-    cin >> languageEXP;
+    for (int i = 0; i < questions_and_answers.size(); i++) { //prints the questions
+        cout << questions_and_answers[i][0] << endl;
 
-    cout << "How many years of experience do you have as a software engineer? ";
-    cout << "1. Less than one year" << endl;
-    cout << "2. One year" << endl;
-    cout << "3. Two years" << endl;
-    cout << "4. Three years" << endl;
-    cout << "5. Over three years" << endl;
-    cout << "6. No experience" << endl;
-    
-    cin >> yearsExperience;
+        for (int j = 1; j < questions_and_answers[i].size(); j++) { //takes answer into vector
+            cout << questions_and_answers[i][j] << endl;
+        }
+
+        cin >> answers[i];
+    }
+  }
+ void setNumProjects(int n) {
+    numProjects = n;
+  }
+
+  int getNumProjects() {
+    return numProjects;
+  }
+
+  void setLanguageEXP(int exp) {
+    languageEXP = exp;
+  }
+
+  int getLanguageEXP() {
+    return languageEXP;
+  }
+
+  void setYearsExperience(int y) {
+    yearsExperience = y;
+  }
+
+  int getYearsExperience() {
+    return yearsExperience;
   }
 };
