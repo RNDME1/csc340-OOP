5714b74177b4d58e3e2259ed4b0759f020d89683
zhengcalvin123 <54451643+zhengcalvin123@users.noreply.github.com>
2023-05-09 23:44:56 -0700

    Update Questionnaire.h

--- Questionnaire.h
+++ Questionnaire.h
@@ -1,1 +1,19 @@
+#ifndef QUESTIONNAIRE_H
+#define QUESTIONNAIRE_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Questionnaire {
+private:
+  int numProjects;
+  int languageEXP;
+  int yearsExperience;
+  vector<int> answers;
+
+public:
+  void askQuestions();
+};
+#endif
