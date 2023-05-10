3b8704dbe11b844aaa36b0ea2239a5a0a0a734b5
David <78831882+daazhou@users.noreply.github.com>
2023-05-09 23:28:57 -0700

    Update Totalscore.cpp

--- Totalscore.cpp
+++ Totalscore.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <iostream>
 using namespace std;
-
+// would require a getScore() function to exist in Questionnaire.cpp
 int calculateTotalScore(const vector<Question> &questions){
   int totalScore = 0;
   for (const Question & q : questions){
